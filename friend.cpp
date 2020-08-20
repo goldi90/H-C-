8f7b97d110feb1628732adba5641049939b70e14
goldi90 <kaushikdwivedi22@gmail.com>
2020-08-20 12:22:27 +0530

    first friend declared

--- friend.cpp
+++ friend.cpp
@@ -7,20 +7,28 @@ public:
      a=n1;
      b= n2;
  }
+ // declaration of  SumComplex as a friend
+ friend complex SumComplex (complex o1, complex o2);
+ 
  void GetNumber(){
      cout<<"Your number is "<<a<<" + "<<b<<"i"<<endl;
  }
 };
 complex SumComplex(complex o1,complex o2){
-    
+    complex o3;
+    o3.Setnumber((o1.a + o2.a),(o1.b + o2.b));
+    return o3;
+
 }
 int main(){
-    complex c1,c2;
+    complex c1,c2,sum ;
     c1.Setnumber(1,4);
-    c2.Setnumber(5,8);
     c1.GetNumber();
+
+    c2.Setnumber(5,8);
     c2.GetNumber();
 
-    
+    sum= SumComplex(c1,c2);
+    sum.GetNumber();
     return 0;
 }
